cfbb7b5e470a31f13f449a4327c35643caf470b2
Kevin <kevin.rodriguez678@gmail.com>
2023-01-12 12:25:00 -0500

    create navbar component

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return ( 
+    <>
+      <header className="header">
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+      </header>
+    </>
+  );
+}
+
+export default NavBar;
